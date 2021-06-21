768238b0fb23e24a1bd7bbedddb5a7197d0ab2b9
jpangal2 <jpangal2@jhu.edu>
2021-06-20 20:41:00 -0400

    First commit

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="grow">
+        hello
+    </div>
+</template>
+
+<script>
+
+    export default {
+      name: 'Card',
+      data: function() {
+        return {
+          hover: true
+        }
+      }
+      
+    }
+  
+</script>
+
+<style scoped>
+
+.grow-enter {
+    transition: all .2s ease-in-out;
+}
+.grow:hover {
+    transition: scale(1.1);
+}
+
+
+</style>
